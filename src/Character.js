fed80dee40c3353ab5d60ba0014648a81e55499a
Jared Smith <jared@regen.network>
2019-05-02 13:23:04 -0600

    removed Math.floor on Character#evaluateIncomingDamage

--- src/Character.js
+++ src/Character.js
@@ -333,7 +333,8 @@ class Character extends Metadatable(EventEmitter) {
    */
   evaluateIncomingDamage(damage, currentAmount) {
     let amount = this.effects.evaluateIncomingDamage(damage, currentAmount);
-    return Math.floor(amount);
+    //jared removed Math.floor to allow for lower level regeneration/damage
+    return amount;
   }
 
   /**
